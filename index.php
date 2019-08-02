83fc866fec8310b888d75d4347c6bebc2e93efa0
seppe_verhavert <seppe.verhavert@gmail.com>
2019-08-02 14:17:01 +0200

    add errors

--- index.php
+++ index.php
@@ -6,12 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Contact</title>
-    <link rel="stylesheet" type = "text/css" href="./css/style.css">
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
 </head>
 
 <body>
     <div class="container">
         <h1>Contact us</h1>
+        <div class="errors">
+            <p> <?php print_r($errors); ?></p>
+        </div>
         <div class="form">
             <form action="" method="post">
                 <p>Full name</p>
@@ -30,9 +33,37 @@
 </html>
 
 <?php
+$errors = array();
 
-echo $_POST["name"]; 
-echo $_POST["email"];
-echo $_POST["message"];
+$name = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
+$email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+$message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
 
+if($name = "" ) {
+    $errors['name'] = "Please fill in your full name.";
+}
+
+if($email = "" ) {
+    $errors['email'] = "Please fill in your email-adress.";
+}
+
+if($message = "" ) {
+    $errors['message'] = "Please fill in a message.";
+}
+
+
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo "Your email is valid";
+} else {
+    $errors['email'] = "This email-address is invalid.";
+}
+
+if (count($errors) > 0) {
+    $errors[0] = "There are mistakes!";
+    exit;
+}
+
+// function displayErrors() {
+    
+// }
 ?>
