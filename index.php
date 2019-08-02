dd9e9382a1bb1bb2522c4b106ff470a2742e833e
seppe_verhavert <seppe.verhavert@gmail.com>
2019-08-02 11:48:31 +0200

    add post method

--- index.php
+++ index.php
@@ -13,16 +13,26 @@
     <div class="container">
         <h1>Contact us</h1>
         <div class="form">
-            <form action="welcome.php" method="post">
-                Full name<input type="text" name="name" placeholder="Full name"><br>
-                Email (*)<input type="text" name="email" placeholder="email"><br>
-                Your message<input type="submit" value="send">
-
-                Welcome <?php echo $_POST["name"]; ?><br>
-                Your email address is: <?php echo $_POST["email"]; ?>
+            <form action="" method="post">
+                <p>Full name</p>
+                <input type="text" name="name" placeholder="Full name"><br>
+                <p>Email (*)</p>
+                <input type="text" name="email" placeholder="Email"><br>
+                <p>Your message</p>
+                <textarea rows="5" cols="30" name="message" placeholder="Write your message here."></textarea>
+                <br>
+                <input type="submit" value="send">
             </form>
         </div>
     </div>
 </body>
 
 </html>
+
+<?php
+
+echo $_POST["name"]; 
+echo $_POST["email"];
+echo $_POST["message"];
+
+?>
